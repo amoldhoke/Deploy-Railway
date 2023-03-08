69ce6944d2c68c19005aaafb5a64d6aa8cd9dda8
amoldhoke <amoldhoke07@gmail.com>
2023-03-08 19:57:08 +0530

    making changes in setting

--- railway/wsgi.py
+++ railway/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'railway.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'railway.settings_rail')
 
 application = get_wsgi_application()
