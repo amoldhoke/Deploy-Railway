cdc94eca19d0e7de48dcef10014dddcd979ab9c2
amoldhoke <amoldhoke07@gmail.com>
2023-03-08 19:52:53 +0530

    initial commit

---
+++ railway/settings_rail.py
@@ -0,0 +1,30 @@
+from railway.settings import *
+from decouple import config
+import os
+
+SECRET_KEY = config('SECRET_KEY')
+
+# ALLOWED_HOSTS = ['web-production-c140.up.railway.app']
+
+# CSRF_TRUSTED_ORIGINS = ['https://web-production-c140.up.railway.app']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
+        'OPTIONS': {'sslmode': 'require'},
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
